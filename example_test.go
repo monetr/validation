8920476ae016191ef4ee467a18a75e7f901a3dc4
Elliot Courant <me@elliotcourant.dev>
2025-01-16 18:43:09 -0600

    feat: Migrating to monetr namespace

--- example_test.go
+++ example_test.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"regexp"
 
-	"github.com/go-ozzo/ozzo-validation/v4"
-	"github.com/go-ozzo/ozzo-validation/v4/is"
+	"github.com/monetr/validation"
+	"github.com/monetr/validation/is"
 )
 
 type Address struct {
